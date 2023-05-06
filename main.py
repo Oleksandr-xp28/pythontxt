4ffcbc4eb9a3150087260554e7219c5754d37072
Oleksandr-xp28 <128472063+Oleksandr-xp28@users.noreply.github.com>
2023-05-07 02:31:39 +0300

    Update main.py
    In this version of the code, the try block encompasses the entire program, including the definition and execution of the main() function. If an exception occurs anywhere in the program, it will be caught by the except block and the error message will be printed to the console.
    
    Note that it's generally not a good idea to catch all exceptions like this because it can make it harder to debug errors in your code. It's better to catch only the specific exceptions that you expect to occur and handle them appropriately.

---
+++ main.py
@@ -0,0 +1,12 @@
+try:
+    # Define the main function to run the program
+    def main():
+        pass
+
+    # Run the main function
+    if __name__ == "__main__":
+        main()
+
+except Exception as e:
+    print(e)
+    
